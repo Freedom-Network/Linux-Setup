0c2d84961e9c19a3f883d8767b6778810fb6ddbe
Freedom-Network <44064609+Freedom-Network@users.noreply.github.com>
2018-10-11 16:05:57 -0600

    Add files via upload

---
+++ FNetARM.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cp cjdns.sh /etc/init.d/cjdns
+chmod +x /etc/init.d/cjdns
+
+#This will download and install the cjdns protocol from source, it might take a few minutes depending on your internet connection.
+/etc/init.d/cjdns install
+#It will ask you about entering peer information into the config file. If you have it, go ahead and enter it. 
+#To enter peers at a later time, stop the editor with [CTRL] + [X] and let the script finish running.
+
+tar xvfz go-ipfs_v0.4.17_linux-arm.tar.gz
+ipfs init
