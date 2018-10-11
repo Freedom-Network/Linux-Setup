ffe58b8d3783bf921dceea5d71a657718e9fb8fc
Freedom-Network <44064609+Freedom-Network@users.noreply.github.com>
2018-10-11 16:10:38 -0600

    Starts CJDNS after installing.

--- FNet64.sh
+++ FNet64.sh
@@ -6,6 +6,7 @@ chmod +x /etc/init.d/cjdns
 /etc/init.d/cjdns install
 #It will ask you about entering peer information into the config file. If you have it, go ahead and enter it. 
 #To enter peers at a later time, stop the editor with [CTRL] + [X] and let the script finish running.
+/etc/init.c/cjdns start
 
-tar xvfz go-ipfs_v0.4.17_linux-amd64.tar.gz
-ipfs init
+tar xvfz go-ipfs_v0.4.17_linux-amd64.tar.gz #Installs IPFS
+ipfs init #Initializes IPFS, all done!
