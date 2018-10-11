e935e0932b5d5637243958ad69f7dd95c717bfa3
Freedom-Network <44064609+Freedom-Network@users.noreply.github.com>
2018-10-11 16:11:19 -0600

    Starts CJDNS after installing

--- FNetARM.sh
+++ FNetARM.sh
@@ -6,6 +6,7 @@ chmod +x /etc/init.d/cjdns
 /etc/init.d/cjdns install
 #It will ask you about entering peer information into the config file. If you have it, go ahead and enter it. 
 #To enter peers at a later time, stop the editor with [CTRL] + [X] and let the script finish running.
+/etc/init.d/cjdns start
 
 tar xvfz go-ipfs_v0.4.17_linux-arm.tar.gz
 ipfs init
